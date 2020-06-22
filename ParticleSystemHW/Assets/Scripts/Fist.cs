0859f3d5d35d41419a2e69d11b35df426badce3f
Oleg Anohin <oanohin@outlook.com>
2020-06-22 21:22:21 +0400

    Particle Systems

---
+++ ParticleSystemHW/Assets/Scripts/Fist.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fist : MonoBehaviour
+{
+  [SerializeField] private ParticleSystem _particle;
+
+  private void Start()
+  {
+    _particle.Stop();
+  }
+
+  private void OnTriggerEnter(Collider other)
+  {
+    if (other.TryGetComponent<Head>(out Head head))
+    {
+      _particle.Play();
+    }
+  }
+}
